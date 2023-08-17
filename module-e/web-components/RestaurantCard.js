2f96351e2c9c5e8dc529001123f49d57b96bb51a
Your Name <you@example.com>
2023-08-17 11:46:49 +0200

    save

--- module-e/web-components/RestaurantCard.js
+++ module-e/web-components/RestaurantCard.js
@@ -1,1 +1,20 @@
 // work here
+/// select event on continue
+
+class RestaurantCard extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.innerHTML = "hello there";
+    const div = document.createElement("div");
+    const style = document.createElement("style");
+    console.log(shadow);
+
+    shadow.appendChild(style);
+    shadow.appendChild(div);
+  }
+}
+
+customElements.define("restaurant-card", RestaurantCard);
