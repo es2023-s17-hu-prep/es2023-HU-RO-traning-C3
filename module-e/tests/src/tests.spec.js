ce2988e6fedb250653cac668e9885e438b012d72
Your Name <you@example.com>
2023-08-17 11:31:42 +0200

    comments

--- module-e/tests/src/tests.spec.js
+++ module-e/tests/src/tests.spec.js
@@ -12,11 +12,17 @@ describe("DiscoutService", function () {
   ];
   describe("getUserDiscount", function () {
     it("retrieves the user discount based on the id", function () {
+      /// given a list of users,
+      /// when the user is found
+      /// return the corresponding discount percentage
       expect(discountService.getUserDiscount(1)).toBe(10);
       expect(discountService.getUserDiscount(2)).toBe(20);
     });
 
     it("returns 0 if the discount is not found", function () {
+      /// given a list of users,
+      /// when the user is not found
+      /// return the a discount percentage of 0
       expect(discountService.getUserDiscount("sometthing random")).toBe(0);
     });
   });
@@ -24,12 +30,16 @@ describe("DiscoutService", function () {
   describe("setUserDiscount", function () {
     it("pushes a new discount to the service", function () {
       var lengthBefore = discountService.userDiscounts.length;
+      /// given a new discount
       var newDiscount = { userId: 5, discountPercentage: 50 };
+      /// when the discount is added
       discountService.setUserDiscount(
         newDiscount.userId,
         newDiscount.discountPercentage
       );
+      /// the length should be greater than before by one
       expect(discountService.userDiscounts.length).toBe(lengthBefore + 1);
+      /// the newly added discount should be equal to `newDiscount`
       expect(
         discountService.userDiscounts[discountService.userDiscounts.length - 1]
       ).toStrictEqual(newDiscount);
@@ -38,10 +48,15 @@ describe("DiscoutService", function () {
 });
 
 describe("UserService", function () {
+  beforeEach(() => {
+    userService.users = [];
+  });
   describe("addUser", () => {
     it("adds a new user with the proper id", function () {
+      /// given an empty user list
+      /// when a user is added
       userService.addUser({ name: "John" });
-      userService.addUser({ name: "Mark" });
+      /// the user should be equal to the one we added, but with the proper id
       expect(userService.users[0]).toStrictEqual({
         name: "John",
         id: 1,
@@ -51,6 +66,13 @@ describe("UserService", function () {
 
   describe("getUserById", () => {
     it("retrieves the right user based on the id", function () {
+      /// given an empty user list
+
+      /// when 2 users are added
+      userService.addUser({ name: "John" });
+      userService.addUser({ name: "Mark" });
+
+      /// getUserById should return the proper one
       expect(userService.getUserById(1)).toStrictEqual({
         name: "John",
         id: 1,
@@ -61,16 +83,20 @@ describe("UserService", function () {
         id: 2,
       });
     });
-    
+
     it("returns nothing when the user is not found", function () {
-      expect(userService.getUserById(20)).toBe(undefined);
+      /// given a list of users, without a user of id 20
+      /// when the user 20 is retrieved
+      var user = userService.getUserById(20);
+      /// it should return nothing
+      expect(user).toBe(undefined);
     });
   });
 });
 
 describe("OrderService", function () {
-  const users = [{ name: "Bob" }];
-  const resp = { data: users };
+  userService.addUser({ name: "John" });
+  userService.addUser({ name: "Mark" });
 
   jest.mock("./DiscountService", () => ({
     getUserDiscount: (id) => {
@@ -99,6 +125,8 @@ describe("OrderService", function () {
 
   describe("placeOrder", () => {
     it("places an order", function () {
+      userService.addUser({ name: "John" });
+      userService.addUser({ name: "Mark" });
       expect(orderService.placeOrder(1, [{ price: 10 }])).toStrictEqual({
         id: 1,
         userId: 1,
